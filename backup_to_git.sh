a0a19d7cd83e4c67c4ec5a770ba65173aec29282
Gert Van Gool <gertvangool@gmail.com>
2012-12-28 11:54:24 +0100

    Run PostgreSQL & MySQL backups if commands are available

--- backup_to_git.sh
+++ backup_to_git.sh
@@ -4,8 +4,14 @@ set -e
 # Set the backup location
 BACKUP_DIR=${BACKUP_DIR:="/tmp/git_backup/"}
 
-# Make MySQL backup
-BACKUP_DIR=$BACKUP_DIR ./mysql.sh
+if [[ $(which mysql_dump &> /dev/null) ]] ; then
+    # Make MySQL backup
+    BACKUP_DIR=$BACKUP_DIR ./mysql.sh
+fi
+if [[ $(which pg_dump &> /dev/null) ]] ; then
+    # Make PostgreSQL backup
+    BACKUP_DIR=$BACKUP_DIR ./postgres.sh
+fi
 # Make file backup
 BACKUP_DIR=$BACKUP_DIR ./copy_files.sh $@
 
